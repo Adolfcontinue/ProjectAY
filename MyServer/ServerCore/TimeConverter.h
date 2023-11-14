793021288cbabde31be6c4371ace2e016bed43d3
Adolfcontinue <62752306+godyello123@users.noreply.github.com>
2023-11-15 00:45:39 +0900

    server

--- MyServer/ServerCore/TimeConverter.h
+++ MyServer/ServerCore/TimeConverter.h
@@ -6,7 +6,17 @@ class TimeConverter
 public:
 	static uint64 CurTime() { return time(NULL); }
 	static uint64 DateTimeToTimeStamp(tm& datetime) { return mktime(&datetime); }
-	
+	static tm TimeStampToDateTime(uint64 src)
+	{
+		struct tm retval;
+		time_t srcval = static_cast<time_t>(src);
+		gmtime_s(&retval, &srcval);
+		return retval;
+	}
+
+	static bool IsExpired(uint64 src) { return src < CurTime(); }
+
+
 	static uint64 AddSeconds(int second, uint64 src = -1)
 	{
 		if (src == -1)	src = CurTime();
@@ -15,17 +25,19 @@ public:
 
 	static uint64 AddMinutes(int min, uint64 src = -1)
 	{
-		if (src == -1)	src = CurTime();
 		return AddSeconds(min * 60, src);
 	}
 
-	static uint64 AddHour(int hour, uint64 src = -1)
+	static uint64 AddHours(int hour, uint64 src = -1)
 	{
+		return AddMinutes(hour * 60, src);
+	}
 
+	static uint64 AddDays(int day, uint64 src = -1)
+	{
+		return AddHours(day * 24, src);
 	}
 
 	
-
-
 };
 
